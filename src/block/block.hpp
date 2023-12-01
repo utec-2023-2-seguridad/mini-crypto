82e5d16fe6456f42f08796bdd1227b4d365be683
Christian Rojas <christian.rojas@utec.edu.pe>
2023-12-01 11:50:30 -0500

    check basic miner collect transactions

--- src/block/block.hpp
+++ src/block/block.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../node/transaction.hpp"
+#include "../node/message/transaction.hpp"
 
 #include <vector>
 #include <chrono>
--- src/miner/collect_transaction.cpp
+++ src/miner/collect_transaction.cpp
@@ -1,3 +1,4 @@
+#include "collect_transaction.hpp"
 #include "mined.hpp"
 
 #include <boost/asio.hpp>
@@ -5,8 +6,10 @@
 namespace mini_crypto::miner
 {
 
-collect_transaction::collect_transaction(const transaction& new_transaction){
-    verify_transaction(new_transaction);
+collect_transaction::do_transactions(std::vector<transaction> txs){
+    for(const auto& tx : txs){
+        verify_transaction(tx);
+    }
 }
 
 void collect_transaction::verify_transaction(const transaction& new_transaction){
@@ -21,13 +24,49 @@ void collect_transaction::verify_transaction(const transaction& new_transaction)
     add_transaction(new_transaction);
 }
 
-void collect_transaction::add_transaction(const transaction& new_transaction){
+void collect_transaction::add_transaction(const transaction& good_transaction){
 
-    transactions.push_back(new_transaction);
+    good_txs.push_back(good_transaction);
 
-    if(transactions.size() == max_transactions_per_block){
-        mined mined(transactions);
+    if(good_txs.size() == max_transactions_per_block){
+        mined mined(good_txs);
     }
+}
+
+void collect_transaction::dump(rapidjson::Writer<rapidjson::StringBuffer>& writer) const
+{
+    writer.StartObject();
+
+    writer.Key("txs");
+    writer.StartArray();
+
+	for(const auto& tx: txs)
+	{
+		tx.dump(writer);
+	}
+
+    writer.EndArray();
+
+    writer.EndObject();
+}
+
+bool collect_transaction::load(const rapidjson::Value& value)
+{
+	if(!value.IsObject())
+		return false;
+
+	if(auto txs = value.FindMember("txs"); txs != value.MemberEnd() && txs->value.IsArray())
+	{
+		for(const auto& tx: txs->value.GetArray())
+		{
+			if(tx.IsObject())
+			{
+				this->txs.emplace_back().load(tx);
+			}
+		}
+    }
+
+    return true;
 }
 
 }
